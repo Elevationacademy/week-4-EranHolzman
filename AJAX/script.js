90d5a6944f286f29878a0ab651ef6a2a833120ba
EranHolzman <eranhol1993@gmail.com>
2020-11-10 16:21:54 +0200

    Small Changes

--- AJAX/script.js
+++ AJAX/script.js
@@ -1,61 +1,64 @@
-// // Exercise 1
+// // // Exercise 1
 
-// const fetch = function (number){
+const fetch = function (number){
 
-// $.ajax({
-//     method: "GET",
-//     url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${number}`,
-//     success: function(result){
-//         console.log(result.items[0].volumeInfo.title) 
-//     },
-//     error: function(xhr, text, error){
-//         console.log("It's an ERROR")
+$.ajax({
+    method: "GET",
+    url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${number}`,
+    success: function(result){
+        console.log(result.items[0].volumeInfo.title) 
+    },
+    error: function(xhr, text, error){
+        console.log("It's an ERROR")
 
-//     }
-// })
-// }
+    }
+})
+}
 
-// fetch(1440633908 )
-
-
-
-// // Exercise 2
+fetch(1440633908 )
 
 
-// const fetch = function (queryType, queryValue){
 
-   
-    
-//     $.get(`https://www.googleapis.com/books/v1/volumes?q=${queryType}:${queryValue}`, function(result){
-      
-//         console.log(result.items[0].volumeInfo) 
-//     })
-    
-//     }
+// Exercise 2
 
+const fetch = function (queryType, queryValue){
+$.ajax({
+        method: "GET",
+        url: `https://www.googleapis.com/books/v1/volumes?q=${queryType}:${queryValue}`,
+        success: function(result){
+            console.log(result.items[0].volumeInfo) 
+        },
+        error: function(xhr, text, error){
+            console.log("It's an ERROR")
 
-// fetch("isbn", 9789814561778)
-// fetch("title", "How to Win Friends and Influence People")
+        }
+    })
+}
+
+fetch("title", "How to Win Friends and Influence People")
+fetch("isbn", 9789814561778)
+
 
 
 // EXERCISE 3
 
 
 const fetch = function (queryType, queryValue){
-    
+
     $.get(`https://www.googleapis.com/books/v1/volumes?q=${queryType}:${queryValue}`, function(result){
 
     const array1 = [result]
     const title = result.items[0].volumeInfo.title
     const authors = result.items[0].volumeInfo.authors
     const ISBN = result.items[0].volumeInfo.industryIdentifiers[0].identifier
-    
-      
+
+
         array1.forEach(function (result) {
             console.log(`The title is: ${title}, The author is- ${authors}. The ISBN IS ${ISBN}.`)
         });
     })
-    
+
     }
 
     fetch("isbn", 9789814561778)
+
